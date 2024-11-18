815e4719e2d139b3b5c09f2acd7394d581867da1
Bohdan <bohdan.chornyi@white-test.com>
2024-11-18 11:50:07 +0200

    debug

--- tests/test-debug.py
+++ tests/test-debug.py
@@ -14,10 +14,3 @@ class TestDebug(BaseTest):
     def test_website_qr_code_create(self, sign_up_fixture, fake_email):
         self.qr_creation_page.website_qr_create()
         self.my_qr_codes_page.expect(self.my_qr_codes_page.locator.sign_up_success_image).to_be_enabled()
-
-    @allure.title(f"PDF QR type - {os.getenv('BROWSER')}")
-    def test_pdf_qr_code_create(self, sign_up_fixture, fake_email):
-        self.qr_creation_page.pdf_qr_create()
-        self.qr_creation_page.click_next_button_step2()
-        self.qr_creation_page.complete_step_3()
-        self.my_qr_codes_page.expect(self.my_qr_codes_page.locator.sign_up_success_image).to_be_enabled()
