b6847fe831f48afafea9b42fc32921a7c47be635
Bohdan <bohdan.chornyi@white-test.com>
2024-11-29 21:22:55 +0200

    qr codes uncommented

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ languages = [
 
 qr_create_methods = [
     "wifi_qr_create",
-    #"instagram_qr_create", "mp3_qr_create", "coupon_qr_create",
-    # "menu_menu_qr_create", "facebook_qr_create", "apps_qr_create", "links_qr_create",
-    # "menu_pdf_qr_create", "pdf_qr_create", "social_media_qr_create", "whatsapp_qr_create",
-    # "video_qr_create", "image_qr_create", "business_qr_create", "vcard_qr_create"
+    "instagram_qr_create", "mp3_qr_create", "coupon_qr_create",
+    "menu_menu_qr_create", "facebook_qr_create", "apps_qr_create", "links_qr_create",
+    "menu_pdf_qr_create", "pdf_qr_create", "social_media_qr_create", "whatsapp_qr_create",
+    "video_qr_create", "image_qr_create", "business_qr_create", "vcard_qr_create"
 ]
 
