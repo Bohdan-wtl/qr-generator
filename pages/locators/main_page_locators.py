ddf96ddf1ed40fbdf564238452795dd0c5f9a6d5
bohdan-wtl <bohdan.chornyi@white-test.com>
2024-12-04 15:20:27 +0200

    fix after creation

--- pages/locators/main_page_locators.py
+++
@@ -1,6 +0,0 @@
-class MainPageLocators:
-
-    def __init__(self, page):
-        self.log_in_button = page.get_by_role("a.-link-log-in.non-draggable")
-        self.sign_up_button = page.get_by_role("a.-link-sign-up.non-draggable")
-        self.main_logo_link = page.locator(".billing-nav .animate-logo")
