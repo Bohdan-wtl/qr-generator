132dd66c6780e0045cbfb55a8b1f75c3470cd1f1
Andrii <andrii.panyk@white-test.com>
2024-12-06 19:55:30 +0200

    udate locator qci

--- pages/locators/register_page_locators.py
+++ pages/locators/register_page_locators.py
@@ -2,4 +2,4 @@ class RegisterPageLocators:
     def __init__(self, page):
         self.email_input_field = page.locator("//input[@id='input-email']")
         self.password_input_field = page.locator("//input[@id='input-password']")
-        self.sign_up_confirm_button = page.locator("//form[@id='register-form']/button[@class='-btn-submit']")
+        self.sign_up_confirm_button = page.locator("//button[@type='button']")
