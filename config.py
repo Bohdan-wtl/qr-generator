8c24ab5ae5023029df88ae9169966c88eb818ca1
Andrii-Pan <andrii.panyk@white-test.com>
2024-12-13 14:20:39 +0200

    Update config.py
    En language changed

--- config.py
+++ config.py
@@ -6,10 +6,11 @@ def get_env(var):
     load_dotenv()
     return os.getenv(var)
 
-languages = ['en'
+languages = [#    'en'
              #    "ar",
              #    "bg",
-             #    "cs", "da", "de", "en",
+             #    "cs",
+                "da", "de",
              #      "bg", "cs", "da", "de", "el", "es", "fi", "fr", "he", "hi", "hr", "hu", "id",
              #      "it", "ja", "ko", "ms", "nl", "no", "pl", "pt", "pt-br", "ro", "sk", "sl", "sr", "sv",
              #      "th", "tr", "uk", "zh-cn", "zh-hk"
